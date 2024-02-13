334270089e81a00f988d62d110a1b7c5ad956750
blackbrokkoli <kastomat@gmail.com>
2024-02-13 17:19:58 +0100

    improve position and rnge of taxi movement

--- scenes/taxi.gd
+++ scenes/taxi.gd
@@ -1,14 +1,17 @@
 extends Sprite2D
 
-var lane = 2
+var lane = 0
 
-const MAX_LANE = 4
-const MIN_LANE = 1
+const MAX_LANE = 2
+const MIN_LANE = -3
 const LANE_HEIGHT = 100
+
+var middle_of_screen
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	middle_of_screen = get_viewport().size.y / 2
+	position.y = middle_of_screen + LANE_HEIGHT * lane
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +19,7 @@ func _process(delta):
 	# move up and down a lane_height on arrow key press (within limits)
 	if Input.is_action_just_pressed("ui_up"):
 		lane = max(lane - 1, MIN_LANE)
-		position.y = LANE_HEIGHT * (lane - 1)
+		position.y = middle_of_screen + LANE_HEIGHT * lane
 	elif Input.is_action_just_pressed("ui_down"):
 		lane = min(lane + 1, MAX_LANE)
-		position.y = LANE_HEIGHT * (lane - 1)
+		position.y = middle_of_screen + LANE_HEIGHT * lane
